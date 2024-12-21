9b34ea5a6d7062c0316d0502efc39bc52fabc6b8
Anurag Pandey <anuragpandey9689@gmail.com>
2024-12-22 00:27:45 +0530

    Added teams and their details on teams page

--- src/components/News/NewsPage.jsx
+++ src/components/News/NewsPage.jsx
@@ -9,11 +9,6 @@ import newsData from "./news.json";
 function NewsSection() {
   return (
     <div className="bg-white py-12 px-6 md:px-20 lg:px-40">
-      <div className="text-center mb-12">
-        <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
-          Game Changers: Top Headlines
-        </h2>
-      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {newsData.map((item, index) => (
           <div
